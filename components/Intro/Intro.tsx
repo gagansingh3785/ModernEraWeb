aedba0b93bcbb7e559996aa068d9afefd16c5f44
gagan <gaganpreet.singh@gojek.com>
2022-12-22 22:32:57 +0530

    [Gagan] checking

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -9,7 +9,7 @@ export const Intro = () => {
         <div className={styles.container}>
             <Image src={img} alt="Tech image" />
             <div className={styles.content}>
-                <h2>We are Developers of ModernEraWeb</h2>
+                <h2>We are fuckers who fuck everyone of ModernEraWeb</h2>
                 <div className={styles.body}>
                     We are software engineers specializing in creating breathtaking digital experiences. We create web applications that are modular, scalable and robust
                 </div>
